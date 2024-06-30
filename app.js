8bf66809b0b4edd8a9bb2c53ba22a5aa608afccc
romeshperera93 <romeshperera123@gmail.com>
2024-06-30 17:34:45 +0530

    second commit

--- app.js
+++ app.js
@@ -2,3 +2,19 @@
     let y=20;
     let z=(x+y);
     console.log(z);
+
+    let customer = {
+        name:"saman",
+        age:20,
+        address:"Gampaha"
+
+    }
+     
+    console.log(customer);
+
+    document.write("Hello Icet")
+    document.getElementById();
+
+    function btnClick(){
+        document.getElementById("h1").innerHTML="saman";
+    }
